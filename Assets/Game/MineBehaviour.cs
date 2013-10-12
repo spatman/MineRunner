a2c96c2ee5bdc8d8ada252938a6414a5315d326d
themortis <majinsilver21@online.de>
2013-10-12 18:19:58 +0200

    architecture prototype

---
+++ Assets/Game/MineBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineBehaviour : MonoBehaviour {
+
+	protected bool isPaused = false;
+	
+	public void Pause()
+	{
+		isPaused = true;	
+	}
+	
+	public void Unpause()
+	{
+		isPaused = false;	
+	}
+}
---
+++ Assets/LevelGenerator/Generator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Generator : MonoBehaviour {
+	
+	[SerializeField] private GameObject[] segmentPrefabs;
+	
+	public List<GameObject> GenerateLevel(int segmentAmount)
+	{
+		Vector2 lastPos = Vector2.zero;
+		List<GameObject> level = new List<GameObject>();
+		
+		for(int i = 0; i < segmentAmount; i++)
+		{
+			int random = Random.Range(0, segmentPrefabs.Length);
+			GameObject newSegment = (GameObject)Instantiate(segmentPrefabs[random], lastPos, Quaternion.identity);
+			lastPos += newSegment.GetComponent<Segment>().GetEndPoint();
+			level.Add(newSegment);
+		}
+		
+		return level;
+	}
+}
